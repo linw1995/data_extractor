ae33fe328f84f04cbdc791db65e65cbf33d64335
林玮 <linw1995@icloud.com>
2019-10-03 00:29:08 +0800

    Support RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Data-Extractor'
-copyright = '2019, 林玮'
-author = '林玮'
+from datetime import date
+
+project = "Data-Extractor"
+year = date.today().year
+copyright = f"{year}, 林玮"
+author = "林玮"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,14 +30,11 @@ author = '林玮'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.githubpages',
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.githubpages"]
 autodoc_inherit_docstrings = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -47,9 +47,17 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+html_theme = "alabaster"
+html_theme_options = {
+    "description": "Combine XPath, CSS Selector and JSONPath for Web data extracting.",
+    "github_user": "linw1995",
+    "github_repo": "data-extractor",
+    "fixed_sidebar": True,
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+html_sidebars = {
+    "**": ["about.html", "navigation.html", "relations.html", "searchbox.html"]
+}
