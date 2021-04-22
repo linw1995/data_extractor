a89dcf89893be36ef2d32c0bffdc84c55adf9e42
林玮 (Jade Lin) <linw1995@icloud.com>
2021-04-22 22:05:32 +0800

    ci: able to export the setup.py file

--- scripts/export_requirements_txt.py
+++ scripts/export_requirements_txt.py
@@ -1,20 +1,27 @@
 # Standard Library
+import enum
 import shlex
 import subprocess
 
 from pathlib import Path
+
+Format = enum.Enum("Format", "requirements setuppy")
 
 
 def fix_end_of_file(text):
     return text.rstrip() + "\n"
 
 
-def pdm_export(args, filename):
+def pdm_export(args, filename, format: Format):
 
     output = subprocess.check_output(
-        shlex.split(f"pdm export -f requirements {' '.join(args)}"), encoding="utf-8"
+        shlex.split(f"pdm export -f {format.name} {' '.join(args)}"), encoding="utf-8"
     )
     output = fix_end_of_file(output)
+    if format is Format.setuppy:
+        output = "\n".join(
+            ['# This a dummy setup.py to enable GitHub "Used By" stats', output]
+        )
 
     p = Path(filename)
     is_new = not p.exists()
@@ -25,13 +32,19 @@ def pdm_export(args, filename):
         raise RuntimeError("Create a new file")
 
 
-pdm_export(args=["--prod"], filename="requirements-mini.txt")
+pdm_export(
+    args=["--prod"], filename="requirements-mini.txt", format=Format.requirements
+)
 pdm_export(
     args=[
         "--prod",
         "-s:all",
     ],
     filename="requirements.txt",
+    format=Format.requirements,
 )
-pdm_export(args=["-s:all"], filename="requirements-dev.txt")
-pdm_export(args=["-s", "docs"], filename="requirements-docs.txt")
+pdm_export(args=["-s:all"], filename="requirements-dev.txt", format=Format.requirements)
+pdm_export(
+    args=["-s", "docs"], filename="requirements-docs.txt", format=Format.requirements
+)
+# pdm_export(args=[], filename="setup.py", format=Format.setuppy)
